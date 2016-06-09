d1fbaeb1cb96ab40721a2ab755a5eaceb2c66f96
Zhang <zhanghuangbin@internal.baidu.com>
2016-06-09 14:46:29 +0800

    add test

--- mTrafficSimulation.py
+++ mTrafficSimulation.py
@@ -5,15 +5,31 @@ fileName   : mTrafficSimulation.py
 author     : zhbbupt
 TODO       : 
 '''
-from TrafficModule.basicTraffic import  basicTraffic
+from TrafficModule.basicTraffic import basicTraffic
 import PropagationModule.propagationModel as pro
 import logging
 import logging.config
 import ConfigParser
-import string, os, sys
-import pdb 
+import string
+import os
+import sys
+import pdb
 from Utils.configTools import loadConfAsDict
 from loadConf import *
+
+import random
+import scipy.stats as ss
+
+
+def standard_normal_rand():
+    while True:
+        X = random.uniform(-3.0, 3.0)
+        Y = random.uniform(0.0, 0.5)
+        if Y < ss.norm.pdf(X):
+            return X
+    plotLine(X,Y,'r','d/km','Loss/dB',title='OkumauraHata')
+
+
 def makeEvironmen(confFile):
     global LOG
     logging.config.fileConfig("logger.conf")
@@ -21,13 +37,12 @@ def makeEvironmen(confFile):
     LOG.debug('This is debug message')
     LOG.info('This is info/ message')
     LOG.warning('This is warning message')
-
-
-if __name__=="__main__":
-    #info=basicTraffic()
+if __name__ == "__main__":
+    # info=basicTraffic()
+    standard_normal_rand()
     cf = ConfigParser.ConfigParser()
     cf.read("common.conf")
-    confDict=loadConfAsDict("eNodeB.conf")
+    confDict = loadConfAsDict("eNodeB.conf")
 
     loadConf("common.conf")
     logging.config.fileConfig("logger.conf")
@@ -35,5 +50,5 @@ if __name__=="__main__":
     LOG.debug('This is debug message2')
     LOG.info('This is info message2')
     LOG.warning('This is warning message2')
-    a=pro.OkumauraHata(1800,10,3)
-    a.plotModel(1,3,0.01)
+    a = pro.OkumauraHata(1800, 10, 3)
+    a.plotModel(1, 3, 0.01)

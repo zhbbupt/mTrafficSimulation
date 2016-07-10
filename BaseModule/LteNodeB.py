422ff6a885ab472ac7bae36f3a3cde1db25c5309
zhbbupt <zhb_bupt@163.com>
2016-07-10 22:17:48 +0800

    update shedule

--- BaseModule/LteNodeB.py
+++ BaseModule/LteNodeB.py
@@ -17,6 +17,28 @@ class LteNodeB(object):
         for traffic in traffic_list : 
             traffic.priority = 0.5 * traffic.level + 0.5 * traffic.delay_tti + traffic.data_size/sum_data + 10 ** ((float)traffic.sinr/10)
 
+    def sort_traffic(self,traffic_a,traffic_b):
+        return cmp(traffic_a.priority,traffic_b.priority)
+
     def shedule(self,traffic_list , lte_base):
         LteSheduler.cal_priority(traffic_list)    
+        traffic_list.sort(self.sort_traffic)
+        self.fail_user = []
+        self.shedule_data = []
+        tmp_fail_user = len(traffic_list)
+        tmp_shedule_data = 0
+        tmp_successs_user = 0
+        # 调用业务
+        for traffic in traffic_list:
+            tmp_shedule_RB = traffic.data_size/self.params.RB_size 
+            if (tmp_successs_user > self.params.max_user) or (tmp_shedule_RB > self.params.RB_num):
+                break
+            else:
+                tmp_shedule_data = tmp_shedule_data + traffic.data_size
+                tmp_successs_user = tmp_successs_user + 1
+        self.shedule_data.append(tmp_shedule_data)
+        tmp_fail_user = tmp_fail_user - tmp_successs_user
+        self.fail_user.append(tmp_fail_user)
         
+
+
